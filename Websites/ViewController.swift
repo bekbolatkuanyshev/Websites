cd5ad1c21db5f8fe907f38b23e2eddf1f256ec4c
Бекболат Куанышев <bekbolatkuanyshev@macbook-pro-bekbolat.local>
2017-02-21 01:01:19 +0600

    Initial Commit

---
+++ Websites/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Websites
+//
+//  Created by Бекболат Куанышев on 21.02.17.
+//  Copyright © 2017 Bekbolat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
